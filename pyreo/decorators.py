9ae79f4922ca49609f80dceebe264805eae1c81c
Thomas Matecki <thomas@marianatek.com>
2020-09-06 12:15:50 -0500

    re-org, circular imports

--- pyreo/decorators.py
+++ pyreo/decorators.py
@@ -1,12 +1,13 @@
 from functools import wraps
 from typing import Any
 
-from pyreo.transactional import ReObjectTransaction
-
 
 def pipelined(func):
     @wraps(func)
     def inner(instance, *args: Any, **kwargs: Any) -> Any:
+
+        from pyreo.transactional import ReObjectTransaction
+
         with ReObjectTransaction(instance._rdict_):
             return func(instance, *args, **kwargs)
 
--- pyreo/mapper.py
+++ pyreo/mapper.py
@@ -4,7 +4,30 @@ from uuid import uuid4
 
 from redis import Redis
 
-from pyreo.transactional import ReObjectTransaction
+
+class ReObjectTransaction:
+    def __init__(self, redis_mapper: "_RedisMapper_") -> None:
+        self.redis_mapper = redis_mapper
+        self._results = None
+
+    def __enter__(self) -> None:
+        self.redis_mapper.push_pipeline()
+        return self
+
+    def __exit__(self, *args, **kwargs) -> None:
+        self._results = self.redis_mapper.pop_pipeline()
+
+    class TransactionNotDone(Exception):
+        pass
+
+    @property
+    def results(self):
+        if self._results is None:
+            raise self.TransactionNotDone
+        return self._results
+
+    def __getitem__(self, idx: int):
+        return self.results[idx]
 
 
 class _RedisMapper_(MutableMapping):
